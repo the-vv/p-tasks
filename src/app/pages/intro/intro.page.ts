e87bf21d09c8bad3dbda74008abf4e39d22a13c6
Vishnu Vinod <vishnuvinod2772001@gmail.com>
2023-12-10 20:59:21 +0530

    intro page done

--- src/app/pages/intro/intro.page.ts
+++ src/app/pages/intro/intro.page.ts
@@ -1,22 +1,39 @@
-import { AfterViewInit, Component } from '@angular/core';
+import { Component, ElementRef, QueryList, ViewChildren } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { AnimationController } from '@ionic/angular/standalone';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-intro',
   templateUrl: './intro.page.html',
   styleUrls: ['./intro.page.scss'],
   standalone: true,
-  imports: [IonicModule, CommonModule, FormsModule]
+  imports: [IonicModule, CommonModule, FormsModule, RouterModule]
 })
-export class IntroPage implements AfterViewInit {
+export class IntroPage {
 
-  constructor() { }
+  @ViewChildren('animateItem', { read: ElementRef }) cardElements!: QueryList<ElementRef<HTMLIonCardElement>>;
 
-  ngAfterViewInit(): void {
-    console
+  constructor(private animationCtrl: AnimationController) { }
+
+  ionViewWillEnter() {
+    this.animateIntro();
   }
 
+  private animateIntro() {
+    const animations = this.cardElements.map((card, index) => {
+      return this.animationCtrl.create()
+        .addElement(card.nativeElement)
+        .duration(400)
+        .iterations(1)
+        .fromTo('transform', 'translateY(50px) scale(0.9)', 'translateY(0px) scale(1)')
+        .fromTo('opacity', '0', '1')
+        .easing('ease-out')
+        .delay((index + 1) * 100);
+    });
+    animations.forEach((animation) => animation.play());
+  }
 
 }
